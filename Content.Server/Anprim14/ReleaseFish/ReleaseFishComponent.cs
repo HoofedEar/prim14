322d552d7f1effa7bbe16ae3405dfc259159d4bb
HoofedEar <HoofedEar@users.noreply.github.com>
2022-05-04 22:30:46 -0700

    water now holds whatever you throw into it

--- Content.Server/Anprim14/ReleaseFish/ReleaseFishComponent.cs
+++ Content.Server/Anprim14/ReleaseFish/ReleaseFishComponent.cs
@@ -1,8 +1,14 @@
-﻿namespace Content.Server.Anprim14.ReleaseFish;
+﻿using Robust.Shared.Containers;
+
+namespace Content.Server.Anprim14.ReleaseFish;
 
 [RegisterComponent]
 [Friend(typeof(ReleaseFishSystem))]
 public sealed class ReleaseFishComponent : Component
 {
-    
+    /// <summary>
+    ///     Container of entities inside this body of water.
+    /// </summary>
+    [ViewVariables] 
+    public Container Container = default!;
 }
