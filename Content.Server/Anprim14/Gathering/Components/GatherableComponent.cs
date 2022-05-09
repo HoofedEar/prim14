dc2ee07c6551a000817521687affbabac015f06a
HoofedEar <HoofedEar@users.noreply.github.com>
2022-05-08 17:24:25 -0700

    parrot gatherable changes

--- Content.Server/Anprim14/Gathering/Components/GatherableComponent.cs
+++ Content.Server/Anprim14/Gathering/Components/GatherableComponent.cs
@@ -8,12 +8,6 @@ namespace Content.Server.Anprim14.Gathering.Components;
 public sealed class GatherableComponent : Component
 {
     /// <summary>
-    ///     Are we using a mapped loot table?
-    /// </summary>
-    [DataField("useMappedLoot")] 
-    public bool UseMappedLoot;
-
-    /// <summary>
     ///     Whitelist for specifying the kind of tools can be used on a resource
     ///     Supports multiple tags.
     /// </summary>
@@ -22,13 +16,6 @@ public sealed class GatherableComponent : Component
     public EntityWhitelist? ToolWhitelist;
 
     /// <summary>
-    ///     Loot table for the resource
-    /// </summary>
-    [DataField("loot")] 
-    public List<EntitySpawnEntry>? Loot = new();
-
-    /// <summary>
-    ///     If this is defined, loot table will must be mapped
     ///     YAML example below
     ///     (Tag1, Tag2, Entity1, Entity2 are placeholders for example)
     ///     --------------------
@@ -43,6 +30,6 @@ public sealed class GatherableComponent : Component
     ///       FishingPole:
     ///         - id: EntityID2
     /// </summary>
-    [DataField("mappedLoot")] 
+    [DataField("loot")] 
     public Dictionary<string, List<EntitySpawnEntry>>? MappedLoot = new();
 }
